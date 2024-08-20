fc8665dbccf3dc59fd15dfa7ef659130b4d6e63c
Tushar Biswas <tusarbiswas888@gmail.com>
2024-08-20 16:16:02 +0600

    fixed api url issue

--- src/redux/api/chatApi.ts
+++ src/redux/api/chatApi.ts
@@ -1,7 +1,7 @@
 import { axiosInstance } from "@/lib/axios";
 
 export const getUsersChatsApi = async () => {
-	const response = await axiosInstance.get("/api/v1//admin/users/messages");
+	const response = await axiosInstance.get("/api/v1/admin/users/messages");
 
 	return response;
 };
