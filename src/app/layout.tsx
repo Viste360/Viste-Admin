6b80f5cdf4494a451be35d49cd8b376e2a99669b
Tushar Biswas <tusarbiswas888@gmail.com>
2024-10-07 04:50:39 +0600

    added microsoft clarity script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 import AuthProvider from "@/providers/AuthProvider";
 import StoreProvider from "@/providers/StoreProvider";
 import ToastProvider from "@/providers/ToastProvider";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,15 @@ export default function RootLayout({
 					</ToastProvider>
 				</StoreProvider>
 				{gaId && <GoogleAnalytics gaId={gaId as string} />}
+				<Script id="clarity-script" strategy="afterInteractive">
+					{`
+					(function (c, l, a, r, i, t, y) {
+						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+					})(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+			`}
+				</Script>
 			</body>
 		</html>
 	);
